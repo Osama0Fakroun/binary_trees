fb5951c32fc7eaa4b0ab12c1883aab9d99f2bab2
kwabenasapong <kwabenasapong@gmail.com>
2022-11-15 13:31:14 +0000

    Update task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth_node = 0;
 
-	if (tree->parent != NULL)
+	if (tree != NULL && tree->parent != NULL)
 	{
 		depth_node = binary_tree_depth(tree->parent) + 1;
 		return (depth_node);
