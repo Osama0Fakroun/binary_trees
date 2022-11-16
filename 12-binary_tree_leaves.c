a9fe418b04e62747c5d1ed03b5c85402bd144ddd
kwabenasapong <kwabenasapong@gmail.com>
2022-11-16 08:30:01 +0000

    Add task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -15,7 +15,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (binary_tree_is_leaf(tree))
+	if (!tree->left && !tree->right)
 		return (1);
 	l_no_leaf = binary_tree_leaves(tree->left);
 	r_no_leaf = binary_tree_leaves(tree->right);
