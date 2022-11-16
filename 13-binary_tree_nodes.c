fcb247bd9455084bcf90d08b80db5f5a25dd250a
kwabenasapong <kwabenasapong@gmail.com>
2022-11-16 08:26:32 +0000

    Add task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_tree_nodes - counts the nodes in a binary tree
+ * with atleast i child
+ * @tree: pointer to the root node of the tree to count
+ * the number of nodes
+ *
+ * Return: size or NULL if tree is empty
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t l_no_node = 0, r_no_node = 0, total_no_node = 0;
+
+	if (!tree)
+		return (0);
+	if ((tree->left || tree->right) && tree->parent)
+		return (1);
+	if (tree->left || tree->right)
+	{
+		l_no_node = binary_tree_nodes(tree->left) + 1;
+		r_no_node = binary_tree_nodes(tree->right) + 1;
+		total_no_node = l_no_node + r_no_node;
+		return (total_no_node - 1);
+	}
+	return (total_no_node);
+	}
